2f72a90ccc71fbc2bcce9e96ee59e2e2ca81fd2c
Hai Nguyen <hai@mayfieldrobotics.com>
2015-08-20 18:33:47 -0700

    added wait to make sure that nodes launch

--- pylaunch/test/test_pylaunch.py
+++ pylaunch/test/test_pylaunch.py
@@ -82,18 +82,26 @@ class TestPylaunch(unittest.TestCase):
                     pl.Param('int_param', 5),
                     pl.Param('command_param', command='echo hello')])
         p.start()
-        int_param = rospy.get_param('int_param')
-        command_param = rospy.get_param('command_param')
 
-        self.assertTrue(type(int_param) == int)
-        self.assertTrue(int_param == 5)
-        self.assertTrue(type(command_param) == str)
-        self.assertTrue(command_param == "hello\n")
-        print "got to shutdown"
-        p.shutdown()
+        rospy.init_node('test')
+        try:
+            msg = rospy.wait_for_message('/chatter', std.String, 10)
+            int_param = rospy.get_param('int_param')
+            command_param = rospy.get_param('command_param')
+
+            self.assertTrue(type(int_param) == int)
+            self.assertTrue(int_param == 5)
+            self.assertTrue(type(command_param) == str)
+            self.assertTrue(command_param == "hello\n")
+        except rospy.exceptions.ROSException as e:
+            self.fail("Failed to launch nodes.")
+        finally:
+            p.shutdown()
 
 
 
 if __name__ == '__main__':
     import rosunit
     rosunit.unitrun('pylaunch', 'test_pylaunch', TestPylaunch)
+    #unittest.main()
+
