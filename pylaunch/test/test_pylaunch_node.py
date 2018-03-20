4041cc75615ae67583f9e85522139f6b99c9fb8b
Peter Baughman <peter@mayfieldrobotics.com>
2018-03-20 20:56:06 +0000

    Add test for new respawn functionality

--- pylaunch/test/test_pylaunch_node.py
+++ pylaunch/test/test_pylaunch_node.py
@@ -1,6 +1,7 @@
 from pylaunch import Node
 
 import os.path as pt
+import random
 import sys
 import unittest
 
@@ -11,18 +12,35 @@ import rosgraph
 class TestPylaunchNode(unittest.TestCase):
 
     def test_pylaunch_node(self):
-        dut = Node(
-            package_name="test_package_name",
-            node_type="test_node_type",
-            node_name="test_node_name"
-        )
-
+        dut = self.build_dut()
         roslaunch_node = self.pylaunch_node_to_roslaunch_node(dut)
 
         self.assertEquals(roslaunch_node.package, "test_package_name")
         self.assertEquals(roslaunch_node.type, "test_node_type")
         self.assertEquals(roslaunch_node.name, "test_node_name")
 
+    def test__respawn(self):
+        dut = self.build_dut()
+        roslaunch_node = self.pylaunch_node_to_roslaunch_node(dut)
+        self.assertEquals(roslaunch_node.respawn, False)
+
+        dut = self.build_dut(respawn=True)
+        roslaunch_node = self.pylaunch_node_to_roslaunch_node(dut)
+        self.assertEquals(roslaunch_node.respawn, True)
+
+        delay = random.randint(10, 100)
+        dut = self.build_dut(respawn=True, respawn_delay=delay)
+        roslaunch_node = self.pylaunch_node_to_roslaunch_node(dut)
+        self.assertEquals(roslaunch_node.respawn, True)
+        self.assertEquals(roslaunch_node.respawn_delay, delay)
+
+    def build_dut(self, **kwargs):
+        return Node(
+            package_name="test_package_name",
+            node_type="test_node_type",
+            node_name="test_node_name",
+            **kwargs
+        )
 
     def pylaunch_node_to_roslaunch_node(self, node):
         launch_config =  roslaunch.config.ROSLaunchConfig()
