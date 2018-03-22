dbafee342021d350389491259b4ae063f0d518bc
Peter Baughman <peter@mayfieldrobotics.com>
2018-03-22 18:10:15 +0000

    Fix up almost all flake8 errors

--- pylaunch/src/pylaunch/__init__.py
+++ pylaunch/src/pylaunch/__init__.py
@@ -1,1 +1,15 @@
-from launch import FInclude, Include, Node, RosParam, Param, PyRosLaunch, LaunchFileRunner, launch, pkg_path, resource_path
+from launch import FInclude, Include, Node, RosParam, Param, PyRosLaunch,\
+    LaunchFileRunner, launch, pkg_path, resource_path
+
+__all__ = [
+    "FInclude",
+    "Include",
+    "LaunchFileRunner",
+    "Node",
+    "Param",
+    "PyRosLaunch",
+    "RosParam",
+    "launch",
+    "pkg_path",
+    "resource_path",
+]
